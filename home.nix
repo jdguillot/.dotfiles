4a53ad36a02f8342843de842253f0bf0d2c45774
jdguillot <cyberfighter@gmail.com>
2024-09-02 22:09:12 -0700

    Moved bash to home.nix

--- home.nix
+++ home.nix
@@ -125,6 +125,18 @@
     ]; 
   };
 
+  ### Bash Shell
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
   ###### Fish Shell
   programs.fish = {
     enable = true;
