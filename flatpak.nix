390117934a632bee21af2e7a7dc50604262740b3
jdguillot <cyberfighter@gmail.com>
2024-09-03 22:45:36 -0700

    Added flatoak.nix

---
+++ flatpak.nix
@@ -0,0 +1,23 @@
+# home.nix
+{ lib, ... }: {
+
+  # nix-flatpak setup
+  services.flatpak.enable = true;
+
+#   services.flatpak.remotes = lib.mkOptionDefault [{
+#     name = "flathub-beta";
+#     location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+#   }];
+
+  services.flatpak.update.auto.enable = false;
+  services.flatpak.uninstallUnmanaged = false;
+  services.flatpak.packages = [
+    #{ appId = "com.brave.Browser"; origin = "flathub"; }
+    "io.github.zen_browser.zen"
+    "org.openscad.OpenSCAD"
+    "org.freecadweb.FreeCAD"
+    "com.usebottles.bottles"
+    "org.libreoffice.LibreOffice"
+  ];
+
+}
