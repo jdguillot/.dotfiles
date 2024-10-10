db0efc08b14be963f8ac97e8af4a6c42ca69c7d3
jdguillot <jdguillot@outlook.com>
2024-10-09 21:18:25 -0700

    Move Neovim config to it's own file

--- hosts/common.nix
+++ hosts/common.nix
@@ -13,6 +13,7 @@
 
   imports = [
     ../programs/lazygit.nix
+    ../programs/neovim.nix
   ];
 
 
@@ -223,69 +224,69 @@
       '';
     };
 
-    ## NeoVim
-    neovim = {
-      enable = true;
-      withPython3 = true;
-      plugins = with pkgs.vimPlugins; [
-        coc-nvim
-        coc-python
-        context-filetype
-        # nerdree
-        neo-tree-nvim
-        fugitive
-        # onedark-vim
-        vim-tmux-navigator
-      ];
-      extraConfig = ''
-        set relativenumber
-        set number
-        set tabstop=2
-        set expandtab
-        set shiftwidth=2
-        set softtabstop=2
-        set wrap
-        set linebreak
-        set list
-        set lcs+=space:·
-        syntax on
-        set ignorecase
-        set smartcase
-        set hlsearch
-        set autoindent
-        set clipboard=unnamedplus
-        nnoremap <C-s> <ESC>:w<CR>
-        nnoremap <C-e> :Neotree filesystem reveal<CR>
-        nnoremap <M-Up> :m -2<CR>
-        nnoremap <M-Down> :m +1<CR>
-        " Coc Nvim
-
-        inoremap <silent><expr> <TAB>
-             \ coc#pum#visible() ? coc#pum#next(1) :
-             \ CheckBackspace() ? "\<Tab>" :
-             \ coc#refresh()
-        inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
-
-        " Use <c-space> to trigger completion
-        if has('nvim')
-          inoremap <silent><expr> <c-space> coc#refresh()
-        else
-          inoremap <silent><expr> <c-@> coc#refresh()
-        endif
-
-        function! CheckBackspace() abort
-          let col = col('.') - 1
-          return !col || getline('.')[col - 1]  =~# '\s'
-        endfunction
-
-        " Use <c-space> to trigger completion
-        if has('nvim')
-          inoremap <silent><expr> <c-space> coc#refresh()
-        else
-          inoremap <silent><expr> <c-@> coc#refresh()
-        endif
-      '';
-    };
+#    ## NeoVim
+#    neovim = {
+#      enable = true;
+#      withPython3 = true;
+#      plugins = with pkgs.vimPlugins; [
+#        coc-nvim
+#        coc-python
+#        context-filetype
+#        # nerdree
+#        neo-tree-nvim
+#        fugitive
+#        # onedark-vim
+#        vim-tmux-navigator
+#      ];
+#      extraConfig = ''
+#        set relativenumber
+#        set number
+#        set tabstop=2
+#        set expandtab
+#        set shiftwidth=2
+#        set softtabstop=2
+#        set wrap
+#        set linebreak
+#        set list
+#        set lcs+=space:·
+#        syntax on
+#        set ignorecase
+#        set smartcase
+#        set hlsearch
+#        set autoindent
+#        set clipboard=unnamedplus
+#        nnoremap <C-s> <ESC>:w<CR>
+#        nnoremap <C-e> :Neotree filesystem reveal<CR>
+#        nnoremap <M-Up> :m -2<CR>
+#        nnoremap <M-Down> :m +1<CR>
+#        " Coc Nvim
+#
+#        inoremap <silent><expr> <TAB>
+#             \ coc#pum#visible() ? coc#pum#next(1) :
+#             \ CheckBackspace() ? "\<Tab>" :
+#             \ coc#refresh()
+#        inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+#
+#        " Use <c-space> to trigger completion
+#        if has('nvim')
+#          inoremap <silent><expr> <c-space> coc#refresh()
+#        else
+#          inoremap <silent><expr> <c-@> coc#refresh()
+#        endif
+#
+#        function! CheckBackspace() abort
+#          let col = col('.') - 1
+#          return !col || getline('.')[col - 1]  =~# '\s'
+#        endfunction
+#
+#        " Use <c-space> to trigger completion
+#        if has('nvim')
+#          inoremap <silent><expr> <c-space> coc#refresh()
+#        else
+#          inoremap <silent><expr> <c-@> coc#refresh()
+#        endif
+#      '';
+#    };
 
     ## Tmux
     tmux = {
---
+++ programs/neovim.nix
@@ -0,0 +1,68 @@
+{ pkgs, ... }:
+{
+
+  programs.neovim = {
+    enable = true;
+    withPython3 = true;
+    plugins = with pkgs.vimPlugins; [
+      coc-nvim
+      coc-python
+      context-filetype
+      # nerdree
+      neo-tree-nvim
+      fugitive
+      # onedark-vim
+      vim-tmux-navigator
+    ];
+    extraConfig = ''
+      set relativenumber
+      set number
+      set tabstop=2
+      set expandtab
+      set shiftwidth=2
+      set softtabstop=2
+      set wrap
+      set linebreak
+      set list
+      set lcs+=space:·
+      syntax on
+      set ignorecase
+      set smartcase
+      set hlsearch
+      set autoindent
+      set clipboard=unnamedplus
+      nnoremap <C-s> <ESC>:w<CR>
+      nnoremap <C-e> :Neotree filesystem reveal<CR>
+      nnoremap <M-Up> :m -2<CR>
+      nnoremap <M-Down> :m +1<CR>
+      " Coc Nvim
+
+      inoremap <silent><expr> <TAB>
+           \ coc#pum#visible() ? coc#pum#next(1) :
+           \ CheckBackspace() ? "\<Tab>" :
+           \ coc#refresh()
+      inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+
+      " Use <c-space> to trigger completion
+      if has('nvim')
+        inoremap <silent><expr> <c-space> coc#refresh()
+      else
+        inoremap <silent><expr> <c-@> coc#refresh()
+      endif
+
+      function! CheckBackspace() abort
+        let col = col('.') - 1
+        return !col || getline('.')[col - 1]  =~# '\s'
+      endfunction
+
+      " Use <c-space> to trigger completion
+      if has('nvim')
+        inoremap <silent><expr> <c-space> coc#refresh()
+      else
+        inoremap <silent><expr> <c-@> coc#refresh()
+      endif
+    '';
+  };
+
+
+}
