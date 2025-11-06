d181832a166fc765503f9e8758c1a048ce3ca680
jonathan-guillot_emcor <jonathan_guillot@emcor.net>
2025-11-05 17:28:16 -0800

    Moved plugin config items to nix config. Was not working before

--- home/features/cli/tmux/default.nix
+++ home/features/cli/tmux/default.nix
@@ -7,11 +7,29 @@
     historyLimit = 100000;
     escapeTime = 300;
     plugins = with pkgs.tmuxPlugins; [
-      catppuccin
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor 'frappe'
+          set -g @catppuccin_window_status_style "slant"
+          set -g @catppuccin_status_background "none"
+          set -g @catppuccin_window_status_style "none"
+          set -g @catppuccin_pane_status_enabled "off"
+          set -g @catppuccin_pane_border_status "off"
+          set -g @catppuccin_directory_text "#{pane_current_path}"
+          set -g @catppuccin_date_time_text "%H:%M:%S"
+        '';
+      }
       # nord
       resurrect
       continuum
-      tmux-floax
+      {
+        plugin = tmux-floax;
+        extraConfig = ''
+          unbind C-t
+          set -g @floax-bind 't'
+        '';
+      }
       vim-tmux-navigator
       {
         plugin = tmux-which-key;
