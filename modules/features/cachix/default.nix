5278b56d4acc868a71041848f7d9677a2a823473
jdguillot <jdguillot@outlook.com>
2025-11-28 22:30:51 -0800

    Trying to exclude cachix if CI

--- modules/features/cachix/default.nix
+++ modules/features/cachix/default.nix
@@ -7,30 +7,37 @@
 
 let
   cfg = config.cyberfighter.features.cachix;
+  isCI = builtins.getEnv "CI" != "";
 in
 {
   options.cyberfighter.features.cachix = {
     enable = lib.mkEnableOption "Enable devenv cachix substituter";
   };
-  config = lib.mkIf cfg.enable {
-    assertions = [
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
       {
-        assertion = config.cyberfighter.features.sops.enable or false;
-        message = "Cachix requires SOPS to be enabled for managing credentials";
+        environment.systemPackages = with pkgs; [
+          cachix
+        ];
       }
-    ];
 
-    environment.systemPackages = with pkgs; [
-      cachix
-    ];
+      (lib.mkIf (!isCI) {
+        assertions = [
+          {
+            assertion = config.cyberfighter.features.sops.enable or false;
+            message = "Cachix requires SOPS to be enabled for managing credentials";
+          }
+        ];
 
-    sops.secrets."cachix-auth-token" = {
-      owner = "${config.cyberfighter.system.username}";
-    };
+        sops.secrets."cachix-auth-token" = {
+          owner = "${config.cyberfighter.system.username}";
+        };
 
-    environment.shellInit = ''
-      export CACHIX_AUTH_TOKEN=$(cat ${config.sops.secrets."cachix-auth-token".path})
-    '';
+        environment.shellInit = ''
+          export CACHIX_AUTH_TOKEN=$(cat ${config.sops.secrets."cachix-auth-token".path})
+        '';
+      })
+    ]
+  );
 
-  };
 }
