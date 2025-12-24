65f4249eae6c9e0f6c33f62f131c96feb39f4ab9
jdguillot <jdguillot@outlook.com>
2025-12-24 13:54:25 -0800

    Fixed Remote Nvim

--- home/modules/features/editor/lazyvim/lua/plugins/remote-nvim.lua
+++ home/modules/features/editor/lazyvim/lua/plugins/remote-nvim.lua
@@ -1,11 +1,14 @@
 return {
 	"amitds1997/remote-nvim.nvim",
-	version = "*", -- Pin to GitHub releases
-
-	dependencies = {
-		"nvim-lua/plenary.nvim", -- For standard functions
-		"MunifTanjim/nui.nvim", -- To build the plugin UI
-		"nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+	lazy = true,
+	cmd = { "RemoteStart", "RemoteStop", "RemoteInfo", "RemoteCleanup", "RemoteConfigDel", "RemoteLog" },
+	keys = {
+		{ "<leader>rs", "<cmd>RemoteStart<cr>", desc = "Remote Start" },
+		{ "<leader>rS", "<cmd>RemoteStop<cr>", desc = "Remote Stop" },
+		{ "<leader>ri", "<cmd>RemoteInfo<cr>", desc = "Remote Info" },
+		{ "<leader>rc", "<cmd>RemoteCleanup<cr>", desc = "Remote Cleanup" },
+		{ "<leader>rd", "<cmd>RemoteConfigDel<cr>", desc = "Remote Config Delete" },
+		{ "<leader>rl", "<cmd>RemoteLog<cr>", desc = "Remote Log" },
 	},
 	config = true,
 }
