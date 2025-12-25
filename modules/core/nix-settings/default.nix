43c36610cc0432613532e4d782c0e982a00edc26
jdguillot <jdguillot@outlook.com>
2025-12-24 17:19:22 -0800

    Adding nix community to cachix

--- modules/core/nix-settings/default.nix
+++ modules/core/nix-settings/default.nix
@@ -53,10 +53,12 @@ in
           substituters = [
             "https://devenv.cachix.org"
             "https://jdguillot.cachix.org"
+            "https://nix-community.cachix.org"
           ];
           trusted-public-keys = [
             "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
             "jdguillot.cachix.org-1:2blGoWA4jRj/xDiez3FqPE5S/RBNtD8uJUCz7weHNcs="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           ];
         })
         {
