d591cc80235ae0a393367983dee41db96c80a18d
Jonathan Guillot <jdguillot@outlook.com>
2026-03-16 21:35:11 -0700

    Adding initial password for new users

--- modules/core/users/default.nix
+++ modules/core/users/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 
@@ -13,12 +12,32 @@ in
     type = lib.types.listOf lib.types.str;
     default = [ ];
     description = "Extra groups for the primary user";
-    example = [ "docker" "libvirtd" ];
+    example = [
+      "docker"
+      "libvirtd"
+    ];
   };
 
   config = {
-    users.users.${cfg.username} = {
-      extraGroups = [ "networkmanager" "wheel" ] ++ cfg.extraGroups;
+
+    sops.secrets."temp-pass" = {
+      neededForUsers = true;
     };
+
+    users.users.${cfg.username} = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBUyIMVw6JsHKA53g8WmxN5gkA0Qy/Gh1lmv8IqiXD5L cyberfighter@razer-nixos"
+      ];
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+      ]
+      ++ cfg.extraGroups;
+      initialHashedPassword = "$y$j9T$ee5r38mtIKQ.eA.TimY7g0$6DUlYVVIus9SD0mPDWDrDeovBmlaVqIJ4/4TKruS1hD";
+
+    };
+
+    # security.sudo.wheelNeedsPassword = false;
+
   };
 }
