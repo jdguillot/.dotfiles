3ccca6e936d36d06b4879c6796cceafc9e57f049
Jonathan Guillot <jdguillot@outlook.com>
2026-04-15 13:50:39 -0700

    Bringing back background for popups in lazy

--- home/modules/features/editor/lazyvim/lua/plugins/colorscheme.lua
+++ home/modules/features/editor/lazyvim/lua/plugins/colorscheme.lua
@@ -8,10 +8,10 @@ return {
 		opts = {
 			flavour = "frappe",
 			transparent_background = true,
-			float = {
-				transparent = true,
-				solid = true,
-			},
+			-- float = {
+			-- 	transparent = true,
+			-- 	solid = true,
+			-- },
 		},
 	},
 	{
