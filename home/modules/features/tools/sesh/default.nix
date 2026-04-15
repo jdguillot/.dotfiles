1f25439b71c39a2149d5f56db9f74cce5dd801ff
Jonathan Guillot <jdguillot@outlook.com>
2026-04-15 09:46:16 -0700

    Adding action to update README with copilot

---
+++ home/modules/features/tools/sesh/default.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.cyberfighter.features.tools.sesh;
+in
+{
+  options.cyberfighter.features.tools.sesh = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Sesh is a terminal session manager written in Rust.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.sesh = {
+      enable = true;
+
+    };
+  };
+}
